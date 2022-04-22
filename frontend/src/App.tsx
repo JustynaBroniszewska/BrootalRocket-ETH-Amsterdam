61814b2e63b4e52708fdb8fa897b730ed2d93187
nezouse <stankiewicz1998@gmail.com>
2022-04-22 22:41:46 +0200

    👕 Add web3auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { Web3Auth } from "@web3auth/web3auth";
+import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
 
 import { useEthers, ChainId } from "@usedapp/core";
+
+const clientId = "YOUR_CLIENT_ID";
 
 const NETWORK_CONNECTIONS = {
   [ChainId.Mainnet]:
@@ -14,9 +18,42 @@ const NETWORK_CONNECTIONS = {
 
 function App() {
   const { account, activateBrowserWallet, deactivate, activate } = useEthers();
+  const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
 
-  const deactivateWallet = () => {
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3auth = new Web3Auth({
+          clientId,
+          chainConfig: {
+            chainId: "0x1",
+            chainNamespace: "eip155",
+            rpcTarget:
+              "https://mainnet.infura.io/v3/ec659e9f6af4425c8a13aeb0af9f2809",
+          },
+        });
+        const openloginAdapter = new OpenloginAdapter({
+          adapterSettings: {
+            clientId,
+            network: "testnet",
+            uxMode: "redirect",
+          },
+        });
+        web3auth.configureAdapter(openloginAdapter);
+        setWeb3auth(web3auth);
+        await web3auth.initModal();
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    init();
+  }, []);
+
+  const deactivateWallet = async () => {
     localStorage.removeItem("walletconnect");
+    if (web3auth) {
+      await web3auth.logout();
+    }
     deactivate();
   };
   return (
@@ -48,6 +85,29 @@ function App() {
         >
           {account ? "Already connected" : "Connect with walletconnect"}
         </button>
+
+        <button
+          onClick={async () => {
+            if (!account) {
+              if (!web3auth) {
+                console.log("web3auth not initialized yet");
+                return;
+              }
+
+              try {
+                const provider = await web3auth.connect();
+                await activate(provider as any);
+              } catch (error) {
+                console.log(error);
+              }
+            } else {
+              deactivateWallet();
+            }
+          }}
+        >
+          {account ? "Already connected" : "Connect with web3auth"}
+        </button>
+
         <button
           onClick={() => {
             account ? deactivateWallet() : activateBrowserWallet();
