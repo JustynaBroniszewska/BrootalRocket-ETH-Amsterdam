b11906b6caa02e0033c5fc9fd8086b77c733fda6
nezouse <stankiewicz1998@gmail.com>
2022-04-23 15:16:58 +0200

    🧢 Export wallet modal to separate file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,29 +3,12 @@ import "./App.css";
 
 import { useEthers } from "@usedapp/core";
 import { useWallet } from "./providers/WalletProvider";
-import {
-  Button,
-  useDisclosure,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalCloseButton,
-  ModalBody,
-  ModalFooter,
-  Grid,
-} from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { WalletModal } from "./components/WalletModal";
 
 function App() {
-  const {
-    activateBrowserWallet,
-    activateWalletConnect,
-    activateWalletLink,
-    activateWeb3Auth,
-    deactivateWallet,
-  } = useWallet();
+  const { deactivateWallet } = useWallet();
   const { account } = useEthers();
-  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <div className="App">
@@ -41,40 +24,7 @@ function App() {
             Disconnect wallet
           </Button>
         ) : (
-          <>
-            <Button colorScheme="blue" onClick={onOpen}>
-              Open Modal
-            </Button>
-            <Modal isOpen={isOpen} onClose={onClose}>
-              <ModalOverlay />
-              <ModalContent>
-                <ModalHeader>Connect wallet</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
-                  <Grid
-                    templateColumns="1fr 1fr"
-                    gap="8px"
-                    columnGap="16px"
-                    rowGap="24px"
-                  >
-                    <Button colorScheme="blue" onClick={activateWalletConnect}>
-                      Walletconnect
-                    </Button>
-                    <Button colorScheme="blue" onClick={activateWalletLink}>
-                      CoinbaseWallet
-                    </Button>
-                    <Button colorScheme="blue" onClick={activateWeb3Auth}>
-                      Web3auth
-                    </Button>
-                    <Button colorScheme="blue" onClick={activateBrowserWallet}>
-                      Metamask
-                    </Button>
-                  </Grid>
-                  <ModalFooter />
-                </ModalBody>
-              </ModalContent>
-            </Modal>
-          </>
+          <WalletModal />
         )}
         <a
           className="App-link"
