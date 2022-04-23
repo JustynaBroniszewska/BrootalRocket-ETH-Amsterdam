80d6f9df6d590b07d4efba9e3b318969c1b3913f
nezouse <stankiewicz1998@gmail.com>
2022-04-23 16:21:49 +0200

    🔗 Setup routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import { Layout } from "./components/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <Layout>
-      <div>App</div>
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
