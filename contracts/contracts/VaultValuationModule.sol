7226fd23db8c80e85f5bbdf9e8d43f7e0418de2b
MiksuJak <mikolaj.jakubowski@protonmail.com>
2022-04-24 00:54:15 +0200

    🦈 Update Vault
    Co-authored-by: Bartek Rutkowski <barrutko.eth@gmail.com>

--- contracts/contracts/VaultValuationModule.sol
+++ contracts/contracts/VaultValuationModule.sol
@@ -3,22 +3,22 @@ pragma solidity ^0.8.10;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-import {FinderInterface} from "./interface/FinderInterface.sol";
 import {OptimisticOracleInterface} from "./interface/OptimisticOracleInterface.sol";
 import {OracleInterfaces} from "./interface/OracleInterfaces.sol";
 
 contract VaultValuationModule {
-    FinderInterface finder;
-    bytes32 priceIdentifier;
-    IERC20 asset;
+    uint256 public constant REQUEST_LIVENESS = 30;
+    bytes32 public constant PRICE_IDENTIFIER = 0x646567656e5f68656176656e5f74657374000000000000000000000000000000;
+
+    OptimisticOracleInterface oracle;
+    IERC20 collateralAsset;
 
     uint256 lastTotalValue;
     uint256 lastTotalValueTimestamp;
 
-    constructor(address _asset, address _finder, bytes32 _priceIdentifier) {
-        asset = IERC20(_asset);
-        finder = FinderInterface(_finder);
-        priceIdentifier = _priceIdentifier;
+    constructor(IERC20 _collateralAsset, OptimisticOracleInterface _oracle) {
+        collateralAsset = _collateralAsset;
+        oracle = _oracle;
     }
 
     function requestTotalValue() public returns (uint256) {
@@ -27,7 +27,7 @@ contract VaultValuationModule {
         return currentTime;
     }
 
-    function updateTotalValueAndTimestamp(uint256 requestTimestamp) public returns (uint256, uint256) {
+    function updateTotalValueAndTimestamp(uint256 requestTimestamp) public returns (uint256) {
         uint256 totalValue = _getOraclePrice(requestTimestamp);
 
         if(requestTimestamp > lastTotalValueTimestamp) {
@@ -35,7 +35,7 @@ contract VaultValuationModule {
             lastTotalValueTimestamp;
         }
 
-        return (totalValue, requestTimestamp);
+        return totalValue;
     }
 
     function getLastTotalValueAndTimestamp() public view returns (uint256, uint256) {
@@ -47,25 +47,20 @@ contract VaultValuationModule {
     //////////////////////////
 
     function _requestOraclePrice(uint256 requestedTime) internal {
-        OptimisticOracleInterface oracle = _getOptimisticOracle();
-        oracle.requestPrice(priceIdentifier, requestedTime, "", asset, 0);
+        oracle.requestPrice(PRICE_IDENTIFIER, requestedTime, "", collateralAsset, 0);
+        oracle.setCustomLiveness(PRICE_IDENTIFIER, requestedTime, "", REQUEST_LIVENESS);
     }
 
     function _getOraclePrice(uint256 requestTimestamp) internal returns (uint256) {
-        OptimisticOracleInterface oracle = _getOptimisticOracle();
         require(
-            oracle.hasPrice(address(this), priceIdentifier, requestTimestamp, ""),
+            oracle.hasPrice(address(this), PRICE_IDENTIFIER, requestTimestamp, ""),
             "Unresolved oracle price"
         );
-        int256 oraclePrice = oracle.settleAndGetPrice(priceIdentifier, requestTimestamp, "");
+        int256 oraclePrice = oracle.settleAndGetPrice(PRICE_IDENTIFIER, requestTimestamp, "");
 
         if (oraclePrice < 0) {
             oraclePrice = 0;
         }
         return uint256(oraclePrice);
-    }
-
-    function _getOptimisticOracle() internal view returns (OptimisticOracleInterface) {
-        return OptimisticOracleInterface(finder.getImplementationAddress(OracleInterfaces.OptimisticOracle));
     }
 }
--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -3,28 +3,37 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from 'hardhat'
+// import { ethers } from 'hardhat'
 
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+import { VaultFactory } from "../build/artifacts"
+import { contract, deploy } from "ethereum-mars"
 
-  // We get the contract to deploy
-  const Factory = await ethers.getContractFactory('VaultFactory')
-  const factory = await Factory.deploy()
+const ORACLE_ADDRESS = '0xa10648Da824330d7C7670E26a234bEf442E77f20'
 
-  await factory.deployed()
+// async function main() {
+//   // Hardhat always runs the compile task when running scripts with its command
+//   // line interface.
+//   //
+//   // If this script is run directly using `node` you may want to call compile
+//   // manually to make sure everything is compiled
+//   // await hre.run('compile');
 
-  console.log('Greeter deployed to:', factory.address)
-}
+//   // We get the contract to deploy
+//   const Factory = await ethers.getContractFactory('VaultFactory')
+//   const factory = await Factory.deploy(ORACLE_ADDRESS)
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error)
-  process.exitCode = 1
+//   await factory.deployed()
+
+//   console.log('Factory deployed to:', factory.address)
+// }
+
+// // We recommend this pattern to be able to use async/await everywhere
+// // and properly handle errors.
+// main().catch((error) => {
+//   console.error(error)
+//   process.exitCode = 1
+// })
+
+deploy({ verify: true }, (deployer, { networkName }) => {
+  contract(VaultFactory, [ORACLE_ADDRESS])
 })
