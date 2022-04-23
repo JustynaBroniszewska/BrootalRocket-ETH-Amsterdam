5e8d5e8fb4e0cc53a9408a79adfdb9d5381ba849
nezouse <stankiewicz1998@gmail.com>
2022-04-23 15:27:53 +0200

    📄 Create initial layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-import { useEthers } from "@usedapp/core";
-import { useWallet } from "./providers/WalletProvider";
-import { Button } from "@chakra-ui/react";
-import { WalletModal } from "./components/WalletModal";
+import { Layout } from "./components/Layout";
 
 function App() {
-  const { deactivateWallet } = useWallet();
-  const { account } = useEthers();
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>{account}</p>
-
-        {account ? (
-          <Button colorScheme="gray" onClick={deactivateWallet}>
-            Disconnect wallet
-          </Button>
-        ) : (
-          <WalletModal />
-        )}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <div>App</div>
+    </Layout>
   );
 }
 
---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+import { Flex, Button, Box, Heading } from "@chakra-ui/react";
+import { useEthers } from "@usedapp/core";
+import { WalletModal } from "./WalletModal";
+import { useWallet } from "../providers/WalletProvider";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export const Layout = ({ children }: LayoutProps) => {
+  const { account } = useEthers();
+  const { deactivateWallet } = useWallet();
+
+  return (
+    <Box minH="full" p="4">
+      <Flex justify="space-between" align="baseline">
+        <Heading>Degen heaven</Heading>
+        {account ? (
+          <Button colorScheme="gray" onClick={deactivateWallet}>
+            Disconnect wallet
+          </Button>
+        ) : (
+          <WalletModal />
+        )}
+      </Flex>
+      <Box py="8">{children}</Box>
+    </Box>
+  );
+};
