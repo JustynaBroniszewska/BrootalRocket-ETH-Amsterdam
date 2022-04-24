9c748d43674b31d246c3080c35788d9c5fbb6d45
JustynaBroniszewska <jusbroniszewska@gmail.com>
2022-04-24 02:07:12 +0200

    💸 Update portfolio

--- frontend/src/pages/Portfolio.tsx
+++ frontend/src/pages/Portfolio.tsx
@@ -1,18 +1,20 @@
 import { gql, useQuery } from "@apollo/client";
-import { useEffect, useState } from "react";
-import { useBlockNumber, useEthers } from "@usedapp/core";
+import { useEffect, useState } from 'react'
+import { useBlockNumber, useContractFunction, useEthers } from '@usedapp/core'
 import { useParams } from "react-router-dom";
-import { Button } from "@chakra-ui/react";
-import axios from "axios";
+import { Button, VStack, Box, FormControl, FormLabel, Select, useTabList, Input } from "@chakra-ui/react";
+import { Contract, utils } from 'ethers'
+import { ASSETS } from "./Create";
 
 export const Portfolio = () => {
-  const address = useParams().address ?? "";
-  const { account } = useEthers();
+
+  const address = useParams().address ?? ''
+  const { account } = useEthers()
   const blockNumber = useBlockNumber();
   const { data, loading, refetch } = useQuery(
     gql`
       query getVaultsById($id: String!) {
-        vault(id: $id) {
+        vault(id: $id)  {
           id
           owner
           asset {
@@ -43,23 +45,75 @@ export const Portfolio = () => {
     getDescription();
   }, [account, data?.vault?.name]);
 
-  return (
-    <div>
-      {data?.vault?.name}
-      <br />
-      {data?.vault?.id}
-      <br />
+  console.log(data)
+  return <div>
+    {data?.vault?.name}
+    <br />
+    {data?.vault?.id}      
+    <br />
       {description}
-      <br />
-      Available protocols:
-      <br />
-      <Button>AAVE</Button>
-      <br />
-      <Button>APWine</Button>
-      <br />
-      <Button>Yearn</Button>
-    </div>
-  );
-};
+    <ManageModal portfolioAddress={data?.vault?.id} />
+  </div>
+}
 
-const ManageModal = () => {};
+
+const ManageModal = ({ portfolioAddress }: { portfolioAddress: string }) => {
+  const [state, setState] = useState('default')
+  if (state == 'aave') {
+    return (<ManageAave portfolioAddress={portfolioAddress} />)
+  }
+
+  return (<VStack>
+    <Box>
+      Available protocols:
+    </Box>
+    <br />
+    <Button onClick={() => setState('aave')}>
+      AAVE
+    </Button>
+    <br />
+    <Button>
+      APWine
+    </Button>
+    <br />
+    <Button>
+      Yearn
+    </Button></VStack>)
+}
+
+const SUPPORTED_AAVE_OPERATIONS = ['deposit', 'borrow', 'withdraw']
+
+const AAVE = '0x794a61358D6845594F94dc1DB02A252b5b4814aD'
+
+const ManageAave = ({ portfolioAddress }: { portfolioAddress: string }) => {
+  const contract = new Contract(AAVE, new utils.Interface(['function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) public']))
+  const { send, state } = useContractFunction(contract, 'supply')
+  console.log(state)
+  return (<VStack
+    as="form"
+    maxW="sm"
+    mt="4"
+    onSubmit={async (e) => {
+      e.preventDefault();
+      const target = e.target as any;
+      console.log('send?', target.amount.value, utils.parseEther(target.amount.value.toString()))
+      await send(ASSETS[0].address, utils.parseEther(target.amount.value.toString()), portfolioAddress, '')
+
+    }}>
+    <FormControl>
+      <FormLabel htmlFor="network">Network:</FormLabel>
+      <Select id="operation">
+        {SUPPORTED_AAVE_OPERATIONS.map((operation) => (
+          <option value={operation}>{operation}</option>
+        ))}
+      </Select>
+      <FormControl>
+        <FormLabel htmlFor="amount">Amount:</FormLabel>
+        <Input id="amount" placeholder="Amount"></Input>
+      </FormControl>
+    </FormControl>
+    <Button type="submit" w="32">
+      Send
+    </Button>
+  </VStack>)
+}
